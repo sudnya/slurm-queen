76cef7a516002fa1e0d1d09706b39c4519495964
sudnya <mailsudnya@gmail.com>
2022-04-26 21:40:47 -0700

    merge conflicts from rebase resolved

--- platform/slurm_controller/scripts/start.sh
+++ platform/slurm_controller/scripts/start.sh
@@ -10,17 +10,10 @@ set -euf -o pipefail
 # Get the directory of this script
 LOCAL_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-<<<<<<< HEAD
 sudo sed -i "s/REPLACE_IT/CPUs=$(nproc)/g" /etc/slurm-llnl/slurm.conf
 
 sudo service munge start
 sudo service slurmctld start
-=======
-sed -i "s/REPLACE_IT/CPUs=$(nproc)/g" /etc/slurm-llnl/slurm.conf
-
-service munge start
-service slurmctld start
->>>>>>> 937fcef (ENH: Added slurm queen endpoints in fastapi.)
 
 # Start the dev environment
 PYTHONPATH=$LOCAL_DIRECTORY/.. uvicorn --host "0.0.0.0" slurm_queen:app --reload
--- platform/slurm_controller/slurm_queen/slurm_cli/router.py
+++ platform/slurm_controller/slurm_queen/slurm_cli/router.py
@@ -1,18 +1,12 @@
-
-<<<<<<< HEAD
 from slurm_queen.util.config import get_config
 
 import subprocess
 
 import tempfile
-=======
-
->>>>>>> 937fcef (ENH: Added slurm queen endpoints in fastapi.)
 import logging
 
 logger = logging.getLogger(__name__)
 
-<<<<<<< HEAD
 def run(job_config: dict):
     logger.info("Run {}".format(job_config))
 
@@ -34,16 +28,10 @@ def run(job_config: dict):
     return {
         "message" : result.stdout
     }
-=======
-def run(config: dict):
-    logger.info("Run {}".format(config))
-
->>>>>>> 937fcef (ENH: Added slurm queen endpoints in fastapi.)
 
 def get_queue():
     logger.info("Get queue")
 
-<<<<<<< HEAD
     config = get_config()
     run_command = ["squeue"]
     result = subprocess.run(run_command, cwd=config["slurm_queen"]["home_path"], stdout = subprocess.PIPE)
@@ -51,13 +39,10 @@ def get_queue():
     return {
         "message" : result.stdout
     }
-=======
->>>>>>> 937fcef (ENH: Added slurm queen endpoints in fastapi.)
 
 def info():
     logger.info("Info")
 
-<<<<<<< HEAD
     config = get_config()
     run_command = ["sinfo"]
     result = subprocess.run(run_command, cwd=config["slurm_queen"]["home_path"], stdout = subprocess.PIPE)
@@ -77,13 +62,11 @@ def status(id: int):
     return {
         "message" : result.stdout
     }
-=======
->>>>>>> 937fcef (ENH: Added slurm queen endpoints in fastapi.)
+
 
 def cancel(id: int):
     logger.info("Cancel {}".format(id))
 
-<<<<<<< HEAD
     config = get_config()
     run_command = ["scancel", str(id)]
     result = subprocess.run(run_command, cwd=config["slurm_queen"]["home_path"], stdout = subprocess.PIPE)
@@ -91,6 +74,3 @@ def cancel(id: int):
     return {
         "message" : result.stdout
     }
-
-=======
->>>>>>> 937fcef (ENH: Added slurm queen endpoints in fastapi.)
